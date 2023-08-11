8e1727c092329f7e35fa99b06b1afcbe3f18adc7
emerson <xinayespinoza@gmail.com>
2023-08-10 23:27:01 -0400

    fix css

--- assets/js/whatsap.js
+++ assets/js/whatsap.js
@@ -1,6 +1,6 @@
 document.getElementById("send-whatsapp").addEventListener("click", function () {
   var message = document.getElementById("whatsapp-message").value;
   var encodedMessage = encodeURIComponent(message);
-  var whatsappURL = "https://api.whatsapp.com/send?phone=+56986323803&text=" + encodedMessage;
+  var whatsappURL = "https://api.whatsapp.com/send?phone=+56940736538&text=" + encodedMessage;
   window.open(whatsappURL, "_blank");
 });
